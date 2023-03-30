27eea50566c7193f14c75a063f476efbf248979e
tlst.ong <tlst.onge@rockwellautomation.com>
2023-03-30 14:40:11 -0600

    first commit

---
+++ ProjectFiles/NetSolution/RuntimeNetLogic1.cs
@@ -0,0 +1,28 @@
+#region Using directives
+using System;
+using UAManagedCore;
+using OpcUa = UAManagedCore.OpcUa;
+using FTOptix.HMIProject;
+using FTOptix.CommunicationDriver;
+using FTOptix.UI;
+using FTOptix.Retentivity;
+using FTOptix.NativeUI;
+using FTOptix.RAEtherNetIP;
+using FTOptix.CoreBase;
+using FTOptix.Core;
+using FTOptix.NetLogic;
+using FTOptix.OPCUAServer;
+#endregion
+
+public class RuntimeNetLogic1 : BaseNetLogic
+{
+    public override void Start()
+    {
+        // Insert code to be executed when the user-defined logic is started
+    }
+
+    public override void Stop()
+    {
+        // Insert code to be executed when the user-defined logic is stopped
+    }
+}
---
+++ ProjectFiles/NetSolution/Runtime_StateMonAliasEmbedded.cs
@@ -0,0 +1,31 @@
+#region Using directives
+using System;
+using UAManagedCore;
+using OpcUa = UAManagedCore.OpcUa;
+using FTOptix.HMIProject;
+using FTOptix.NetLogic;
+using FTOptix.UI;
+using FTOptix.NativeUI;
+using FTOptix.OPCUAServer;
+using FTOptix.RAEtherNetIP;
+using FTOptix.Retentivity;
+using FTOptix.CommunicationDriver;
+using FTOptix.CoreBase;
+using FTOptix.Core;
+#endregion
+
+public class Runtime_StateMonAliasEmbedded : BaseNetLogic
+{
+    public override void Start()
+    {
+        // Insert code to be executed when the user-defined logic is started
+        string origNode = Owner.GetVariable("AliasPath").Value;
+        var newNode = Project.Current.Get("CommDrivers" + origNode.Split("CommDrivers")[1] + "_StateMon");
+        Owner.Get<Alias>("StateMon_Alias").Value = newNode.NodeId;
+    }
+
+    public override void Stop()
+    {
+        // Insert code to be executed when the user-defined logic is stopped
+    }
+}
